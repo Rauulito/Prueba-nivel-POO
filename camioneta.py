3726808ec73954721c0b3cc12125c2148b6c1f0b
Rauulito <7rnavarro7@gmail.com>
2022-09-13 12:25:24 +0200

    clase camioneta terminada

--- camioneta.py
+++ camioneta.py
@@ -1,15 +1,16 @@
 
-from Coche import coche
+from Coche import Coche
 
 #creamos la clase camioneta
-class camioneta(coche):
-    def __init__(self, color, ruedas, velocidad, cilindrada, carga):
-        coche.__init__(self, color, ruedas, velocidad, cilindrada)
+class Camioneta(Coche):
+    def __init__(self, color, ruedas, velocidad, cilindrada, carga): #Se heredan los atributos de la clase coche y definimos los adicionales de la clase camioneta
+        Coche.__init__(self, color, ruedas, velocidad, cilindrada)
         self.carga=carga
 
-    def __str__(self):
-        return coche.__str__(self) + " {} kg".format(self.carga)
+    def __str__(self): #preparamos el formato de salida
+        return Coche.__str__(self) + " {} kg".format(self.carga)
 
 #prueba
-c = camioneta("azul", 4, 150, 1200,500)
-print(c)
+if __name__== '__main__':
+    c = Camioneta("azul", 4, 150, 1200,500)
+    print(type(c).__name__,":",c)
