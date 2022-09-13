4b35612dcd5cf624716a2b9e1d362ca79192e58d
Rauulito <7rnavarro7@gmail.com>
2022-09-13 12:34:29 +0200

    clase motocicleta terminada

--- motocicleta.py
+++ motocicleta.py
@@ -1,14 +1,15 @@
-from Vehiculo import vehiculo
-class motocicleta(vehiculo):
-    def __init__(self, color, ruedas,tipo, velocidad, cilindrada):
-        vehiculo.__init__(self, color, ruedas)
-        self.tipo=tipo
+from Bicicleta import Bicicleta
+#creamos la clase Motocicleta
+class Motocicleta(Bicicleta):
+    def __init__(self, color, ruedas, tipo, velocidad, cilindrada): #Se heredan los atributos de la clase Bicleta y definimos los adicionales de la clase Motocicleta
+        Bicicleta.__init__(self, color, ruedas, tipo)
         self.velocidad = velocidad
         self.cilindrada = cilindrada
 
-    def __str__(self):
-        return vehiculo.__str__(self) + ", {}, {} km/h, {}cc".format(self.tipo,self.velocidad, self.cilindrada)
+    def __str__(self): #preparamos el formato de salida
+        return Bicicleta.__str__(self) + " {} km/h, {}cc".format(self.velocidad, self.cilindrada)
 
 #prueba
-c = motocicleta("verde", 2,"urbana", 120, 1100)
-print(c)
+if __name__== '__main__':
+    c = Motocicleta("verde", 2,"urbana", 120, 1100)
+    print(type(c).__name__,":",c)
